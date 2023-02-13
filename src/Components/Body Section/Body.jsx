4d5ea8d1913ec4e8988de3bdc79462c2f5c2e296
Yesid200228 <yesidwork2804@gmail.com>
2023-02-12 22:09:25 -0500

    cambio nombre de carpetas

--- src/Components/Body Section/Body.jsx
+++ src/Components/Body Section/Body.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./body.css";
-import Top from "./Top Section/Top";
-import Listing from "./Listing Section/Listing";
-import Activity from "./Activity Section/Activity";
+import Top from "./Top_Section/Top";
+import Listing from "./Listing_Section/Listing";
+import Activity from "./Activity_Section/Activity";
 
 const Body = () => {
   return (
